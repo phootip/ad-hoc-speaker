4834ae9222c0b69020ce4a24ac0b6ea6931d852f
Poomchai Taechaprapasrat <poomchai@Poomchais-MacBook-Pro.local>
2018-11-09 13:58:37 +0700

    Add listener and speaker

---
+++ listener.py
@@ -0,0 +1,46 @@
+import socket
+import pyaudio
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 4
+WAVE_OUTPUT_FILENAME = "server_output.wav"
+WIDTH = 2
+frames = []
+
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+# client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client.bind(('', 12345))
+print('wait')
+
+i=1
+while True:
+    print(1)
+    data, addr = client.recvfrom(1024)
+    stream.write(data)
+    frames.append(data)
+
+
+
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+conn.close()
---
+++ speaker.py
@@ -0,0 +1,41 @@
+import socket
+import pyaudio
+import wave
+
+#record
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 40
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("*recording")
+
+frames = []
+
+for i in range(0, int(RATE/CHUNK*RECORD_SECONDS)):
+    data  = stream.read(CHUNK)
+    frames.append(data)
+    server.sendto(data, ('255.255.255.255', 12345))
+
+print("*done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+server.close()
+
+print("*closed")
